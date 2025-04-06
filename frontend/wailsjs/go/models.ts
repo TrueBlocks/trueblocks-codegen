0ba96436888d24d1ebb07219882a8f48c8274e0b
Thomas Jay Rush <jrush@quickblocks.io>
2025-04-05 20:02:33 -0400

    Best version yet

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,17 +1,161 @@
-export namespace config {
+export namespace app {
 	
-	export class Preferences {
+	export class FileStatus {
+	    name: string;
+	    dirty: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new FileStatus(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.name = source["name"];
+	        this.dirty = source["dirty"];
+	    }
+	}
+
+}
+
+export namespace keys {
+	
+	export class Accelerator {
+	    Key: string;
+	    Modifiers: string[];
+	
+	    static createFrom(source: any = {}) {
+	        return new Accelerator(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Key = source["Key"];
+	        this.Modifiers = source["Modifiers"];
+	    }
+	}
+
+}
+
+export namespace menu {
+	
+	export class Menu {
+	    Items: MenuItem[];
+	
+	    static createFrom(source: any = {}) {
+	        return new Menu(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Items = this.convertValues(source["Items"], MenuItem);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class MenuItem {
+	    Label: string;
+	    Role: number;
+	    Accelerator?: keys.Accelerator;
+	    Type: string;
+	    Disabled: boolean;
+	    Hidden: boolean;
+	    Checked: boolean;
+	    SubMenu?: Menu;
+	
+	    static createFrom(source: any = {}) {
+	        return new MenuItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Label = source["Label"];
+	        this.Role = source["Role"];
+	        this.Accelerator = this.convertValues(source["Accelerator"], keys.Accelerator);
+	        this.Type = source["Type"];
+	        this.Disabled = source["Disabled"];
+	        this.Hidden = source["Hidden"];
+	        this.Checked = source["Checked"];
+	        this.SubMenu = this.convertValues(source["SubMenu"], Menu);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class CallbackData {
+	    MenuItem?: MenuItem;
+	
+	    static createFrom(source: any = {}) {
+	        return new CallbackData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.MenuItem = this.convertValues(source["MenuItem"], MenuItem);
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
+
+}
+
+export namespace types {
+	
+	export class Bounds {
 	    x: number;
 	    y: number;
 	    width: number;
 	    height: number;
-	    lastFile: string;
-	    lastView: string;
-	    menuCollapsed: boolean;
-	    helpCollapsed: boolean;
 	
 	    static createFrom(source: any = {}) {
-	        return new Preferences(source);
+	        return new Bounds(source);
 	    }
 	
 	    constructor(source: any = {}) {
@@ -20,11 +164,51 @@ export namespace config {
 	        this.y = source["y"];
 	        this.width = source["width"];
 	        this.height = source["height"];
+	    }
+	}
+	export class AppPreferences {
+	    version: string;
+	    name: string;
+	    bounds: Bounds;
+	    recently_used_files: string[];
+	    lastFile: string;
+	    lastView: string;
+	    menuCollapsed: boolean;
+	    helpCollapsed: boolean;
+	
+	    static createFrom(source: any = {}) {
+	        return new AppPreferences(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.version = source["version"];
+	        this.name = source["name"];
+	        this.bounds = this.convertValues(source["bounds"], Bounds);
+	        this.recently_used_files = source["recently_used_files"];
 	        this.lastFile = source["lastFile"];
 	        this.lastView = source["lastView"];
 	        this.menuCollapsed = source["menuCollapsed"];
 	        this.helpCollapsed = source["helpCollapsed"];
 	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 
 }
